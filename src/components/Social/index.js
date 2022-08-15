0838050df226e69c2a097fd6bb69d7dd09cf9e09
MohammadTaseenKhan <mohammadtaseenkhan2004@gmail.com>
2022-08-15 10:07:48 +0600

    u

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import Wrapper from "./Wrapper";
-import SocialLink from "./SocialLink";
+import Wrapper from './Wrapper';
+import SocialLink from './SocialLink';
 
 function Social({ website, twitter, github, linkedin }) {
   return (
@@ -43,17 +43,22 @@ function Social({ website, twitter, github, linkedin }) {
           Twitter
         </SocialLink>
       )}
-      {facebook && (
-        <SocialLink href={facebook} target="_blank" rel="noopener">
+      {github && (
+        <SocialLink href={github} target="_blank" rel="noopener">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
             viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
-            <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z" />
+            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
           </svg>
-          Facebook
+          Github
         </SocialLink>
       )}
       {linkedin && (
